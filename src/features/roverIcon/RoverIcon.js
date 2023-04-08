c7434764928bc27b9bf1deafdf3db844b6178969
Barbie Ordonez <ordonezbarbie@gmail.com>
2023-04-08 13:53:29 -0600

    Add rover images

--- src/features/roverIcon/RoverIcon.js
+++ src/features/roverIcon/RoverIcon.js
@@ -1,11 +1,22 @@
-import spiritImg from '../../assets/spirit.png'
-import './RoverIcon.css'
+import Spirit from '../../assets/Spirit.png';
+import Opportunity from '../../assets/Opportunity.png';
+import Perseverance from '../../assets/Perseverance.png';
+import Curiosity  from '../../assets/Curiosity.png';
+import './RoverIcon.css';
 
 function RoverIcon({rovers}) {
+
+  const imgObj = {
+    Spirit : Spirit,
+    Opportunity : Opportunity,
+    Perseverance : Perseverance,
+    Curiosity : Curiosity
+  }
+
   const roversIcons = rovers.map(rover => {
     return (
       <div className='rover-icon' key={rover.id}>
-        <img src={spiritImg} className='rover-icon-img'/>
+        <img src={imgObj[rover.name]} className='rover-icon-img'/>
         <p>{rover.name}</p>
       </div>
     )
