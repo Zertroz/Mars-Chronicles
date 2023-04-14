51376a416208e4d122186dc517c600746b16e63d
Barbie Ordonez <ordonezbarbie@gmail.com>
2023-04-14 15:16:30 -0600

    Add modal component and test an image in rover page '

--- src/features/Gallery/Gallery.js
+++ src/features/Gallery/Gallery.js
@@ -2,18 +2,22 @@ import React from 'react';
 import {useSelector} from 'react-redux';
 import './Gallery.css';
 import UserMessage from '../UserMessage/UserMessage';
+import Modal from '../Modal/Modal';
 
 function Gallery () {
   const roverImages = useSelector(state => state.root.images)
   const roverTiles = roverImages.map((img) => <img src={img.img_src} alt={`Rover shot ${img.id}`} className="gallery-tile" key={img.id}/>)
 
   return (
-    roverImages.length ? 
+    <>
+    {roverImages.length ? 
       <section className="gallery-grid">
         {roverTiles}
       </section>
       :
-      <UserMessage message={'Select Sol or Date...'}/>
+      <UserMessage message={'Select Sol or Date...'}/>}
+      <Modal />
+    </>
   )
 }
 
---
+++ src/features/Modal/Modal.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Modal.css';
+
+const Modal = () => {
+  
+  return (
+    <div className='modal'>
+      {/* <img className='modalImg' src='https://mars.nasa.gov/mars2020-raw-images/pub/ods/surface/sol/00100/ids/edr/browse/ncam/NLF_0100_0675828767_506ECM_N0040218NCAM00503_01_295J01_1200.jpg'/> */}
+      <img className='modal-img' src='https://mars.nasa.gov/mars2020-raw-images/pub/ods/surface/sol/00100/ids/edr/browse/meda/WSM_0100_0675791183_000ECM_N0040218MEDA03245_0000LUJ01_1200.jpg'/>
+    </div>
+  )
+
+}
+
+export default Modal;
